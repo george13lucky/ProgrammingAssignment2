dc0f483b52190e2efee81213eec2c92b6d37191c
george beirne <george.beirne@ucdconnect.ie>
2019-07-18 21:40:47 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## functions that determine if a matrix inverse has been previously
+## caluclated and if so, then used cached version to save computation and demonstrate
+## scoping in R
+
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Function creates a special “matrix", containing a list of functions to:
+## set and get the value of the matrix, and set , get the value of the matrix inverse
 
+
+makeCacheMatrix <- function(x = matrix()) {
+  ##  create a variable object to store inverse
+  ## initialize the value of the matrix inverse variable  to NULL
+  invers <- NULL
+  ##  delcare 'set' function set to store value that is to be cached in 1. 
+  set <- function(y) {
+    x <<- y
+    invers <<- NULL ## reset inverse matrix variable to null.
+  }
+  get <- function() x
+  setInvr <- function(solve) invers <<- solve # r 'solve' function to calculate inverse of a square matrix
+  getInvr <- function() invers
+  list(set = set,get = get, setInvr = setInvr,getInvr = getInvr)
 }
 
 
-## Write a short comment describing this function
-
+## function to determine if inverse matrix object has been created and if so
+## then use the cached matrix version function and reduce computation
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInvr()
+  ## if this is not the first call to this matrix inverse then used cached function
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  res_matrix <- x$get()
+  inv <- solve(res_matrix, ...)   ## use R 'solve' function to calculate square matrix inverse
+  x$setInvr(inv)
+  inv
 }
